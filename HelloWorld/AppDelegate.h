76289ddc614b8e3661161c38fccf84aedff056cc
hank24 <mobim@MobimMacdeMacBook-Pro.local>
2017-06-13 15:39:33 +0800

    HelloWorld
    添加project

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by MobimMac on 2017/6/13.
+//  Copyright © 2017年 faker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by MobimMac on 2017/6/13.
+//  Copyright © 2017年 faker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
